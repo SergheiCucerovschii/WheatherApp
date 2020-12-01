7208ecaf846a5e63d8fa83ee474ce08f89f281be
SergheiCucerovschii <74251573+SergheiCucerovschii@users.noreply.github.com>
2020-12-01 23:01:49 +0200

    Update weatherApp.py

--- weatherApp.py
+++ weatherApp.py
@@ -1,10 +1,6 @@
 from http.client import HTTPSConnection
 from json import loads
 
-key = "eef9a6e3a696817d913d2df9e2ba51ba"
-city = input("What location you want?").capitalize()
-domain = "api.openweathermap.org"
-endpoint_current = f"/data/2.5/weather?q={city}&appid={key}&units=metric&lang=ru"
 
 def menu(title, options):
     print(f'##### {title:^15} #####')
@@ -16,7 +12,13 @@ def menu(title, options):
         print('Wrong option!')
     return selected
 
-def connection():
+
+def loadData():
+    key = "d54d475a90909b2e41a6011fa52a352f"
+    city = input("Enter the city to see weather:")
+    domain = "api.openweathermap.org"
+    endpoint_current = f"/data/2.5/weather?q={city}&appid={key}&units=metric"
+
     connection = HTTPSConnection(domain)
     connection.request("GET", endpoint_current)
     response = connection.getresponse()
@@ -24,36 +26,51 @@ def connection():
     connection.close()
     return data
 
-def temperature(connection):
-    temp = connection()['main']['temp']
-    print(f"TEMPERATURE in {connection()['name']}: {temp}℃")
 
-def wind(connection):
-    wind = connection()['wind']['speed']
-    print(f"WIND SPEED in {connection()['name']}: {wind}m/s")
+def process_dataTemp(data):
+    temp = data['main']['temp']
+    return temp
 
-def weather(connection):
-    weather = connection()['weather'][0]['description']
-    print(f"WEATHER in {connection()['name']}: {weather}")
 
+def process_dataWind(data):
+    wind = data['wind']['speed']
+    return wind
+
+
+def process_dataWeather(data):
+    weather = data['weather'][0]['description']
+    return weather
+
+
+def process_dataLocation(data):
+    location = data['name']
+    return location
+
+
+def printData(temp, wind, weather, location):
+    print(f"Temperature in {location}: {temp}°C")
+    print(f"Wind speed  in {location}: {wind}m/s")
+    print(f"Weather     in {location}: {weather}")
+
+
+###############################################################
 
 while True:
-    result = menu('Weather App', {
-        1: 'Temperature',
-        2: 'Weather',
-        3: 'Wind ',
+    result = menu("Main", {
+        1: 'Weather in my town.',
         0: 'Exit'
     })
-
     if result == 1:
-        data = connection()
-        data = temperature(connection)
-
-    if result == 2:
-        data = connection()
-        data = weather(connection)
-    if result == 3:
-        data = connection()
-        data = wind(connection)
-    if result == 0:
+        data = loadData()
+        if data['cod'] == 200:
+            temp = process_dataTemp(data)
+            wind = process_dataWind(data)
+            weather = process_dataWeather(data)
+            location = process_dataLocation(data)
+            printData(temp, wind, weather, location)
+        else:
+            print("Error! City not found")
+    elif result == 0:
         print("Thank you!")
+        break
+
